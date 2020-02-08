b9c4d1fec75622892e18c4ef0f410833c3f745be
Kenyawest <judegantony@gmail.com>
2020-02-07 21:48:43 -0500

    First commit with majority done

---
+++ Assets/CameraLock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing;
+
+    Vector3 offset;
+    float lowY, highY, lowX, highX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+        lowY = transform.position.y;
+        highY = transform.position.y + 490;
+        lowX = transform.position.x ;
+        highX = transform.position.x + 850;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetCanPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCanPos, smoothing * Time.fixedDeltaTime);
+
+
+        if (transform.position.y < lowY) transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
+        if (transform.position.y > highY) transform.position = new Vector3(transform.position.x, highY, transform.position.z);
+
+        if (transform.position.x < lowX) transform.position = new Vector3(lowX, transform.position.y, transform.position.z);
+        if (transform.position.x > highX) transform.position = new Vector3(highX, transform.position.y, transform.position.z);
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float maxSpeed;
+
+    //jumping
+    bool grounded = false;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
+
+    Rigidbody2D rgdbody;
+    Animator myAnim;
+    bool facingRight;
+    // Start is called before the first frame update
+    void Start() {
+        rgdbody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+
+        facingRight = false;
+        
+    }
+
+    private void Update()
+    {
+        if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            myAnim.SetBool("isGrounded", grounded);
+            rgdbody.AddForce(new Vector2(0, jumpHeight));
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+
+
+        //check grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        myAnim.SetBool("isGrounded", grounded);
+        myAnim.SetFloat("verticalSpeed", rgdbody.velocity.y);
+
+
+        float movement = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("speed", Mathf.Abs(movement));
+
+        rgdbody.velocity = new Vector2(movement*maxSpeed, rgdbody.velocity.y);
+
+        if (movement > 0 && !facingRight)
+        {
+            flip();
+        } else if (movement < 0 && facingRight)
+        {
+            flip();
+        }
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
